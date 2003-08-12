9b2f739475df5a43dd5ea6358c5591740172bdbd
jaredy <>
2003-08-12 17:52:13 +0000

    renamed report.c to bark.c

--- bark.c
+++ bark.c
@@ -1,24 +1,36 @@
 /* $Id$ */
 
-#include "qpopauthd.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "qpopauthd.h"
 
 void carp(char *fmt, ...)
 {
 	va_list ap;
+	extern int errno;
+
 	va_start(ap, fmt);
 	(void)vfprintf(stderr, fmt, ap);
-	(void)fprintf(stderr, "\n");
 	va_end(ap);
+	(void)fprintf(stderr, "\n");
+
+	if (errno)
+		perror(NULL);
+
+	exit(1);
 }
 
-void warn(char *fmt, ...)
+void bark(char *fmt, ...)
 {
 	va_list ap;
-	va_start(ap, fmt);
+
 	(void)fprintf(stderr, "[DEBUG] ");
+
+	va_start(ap, fmt);
 	(void)vfprintf(stderr, fmt, ap);
-	(void)fprintf(stderr, "\n");
 	va_end(ap);
+
+	(void)fprintf(stderr, "\n");
 }
